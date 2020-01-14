bd02d1fb5a7f1636dc94e4b9ad1a24f2dbc9c728
akanksha606 <akankshagupta98583@gmail.com>
2020-01-14 23:53:35 +0530

    Megabox

--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.movies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton floatingActionButton;
+    BottomAppBar bottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingActionButton=findViewById(R.id.fab);
+        bottomAppBar=findViewById(R.id.bottonAppBar);
+        //main line for setting bottomAppBar
+        setSupportActionBar(bottomAppBar);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Fab is clicked!",Snackbar.LENGTH_LONG).setAction("UNDO",null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_app_bar,menu);
+        return true;
     }
 }
