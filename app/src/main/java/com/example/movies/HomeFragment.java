0c427e2cc762fe5b40459600073b121cb3fa4a84
akanksha606 <akankshagupta98583@gmail.com>
2020-02-02 12:43:10 +0530

    Megabox

--- app/src/main/java/com/example/movies/HomeFragment.java
+++ app/src/main/java/com/example/movies/HomeFragment.java
@@ -11,35 +11,24 @@ import com.android.volley.RequestQueue;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
-    List<Movie> movieList;
-    RecyclerView recyclerView;
-    RequestQueue movieQueue;
-    private Context mContext;
 
-
-     private String url=null;
-
+    @Nullable
     @Override
-    public view onCreate(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
-        super.onCreate(savedInstanceState);
-
-        recyclerView=view.findViewById(R.id.recycler_view_home);
-        movieList=new ArrayList<>();
-
-
-
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,null);
 
     }
-
-
-
-
 }
+
+
+
+
+
 
